3392b0bfa8db9bdbb0bfc6fe7baa6d1fefec3172
Connor Power <connor@connorpower.com>
2020-01-12 16:02:56 +0000

    Animate pig eyes and mouth in response to face changes

--- ARFaceCase/ARFaceCase/Content/Pig.swift
+++ ARFaceCase/ARFaceCase/Content/Pig.swift
@@ -15,7 +15,48 @@ class Pig: SCNNode {
     private let occlusionNode: SCNNode
 
     private var neutralBrowY: Float = 0
+
+    private var neutralMouthY: Float = 0
+
+    private var neutralRightPupilX: Float = 0
+    private var neutralRightPupilY: Float = 0
+
+    private var neutralLeftPupilX: Float = 0
+    private var neutralLeftPupilY: Float = 0
+
     private lazy var browNode = childNode(withName: "brow", recursively: true)!
+
+    private lazy var mouthNode = childNode(withName: "mouth", recursively: true)!
+
+    private lazy var eyeRightNode = childNode(withName: "eyeRight", recursively: true)!
+    private lazy var pupilRightNode = childNode(withName: "pupilRight", recursively: true)!
+
+    private lazy var eyeLeftNode = childNode(withName: "eyeLeft", recursively: true)!
+    private lazy var pupilLeftNode = childNode(withName: "pupilLeft", recursively: true)!
+
+    // MARK: - Private Computed Properties
+
+    private lazy var browHeight: Float = {
+        let (min, max) = browNode.boundingBox
+        return max.y - min.y
+    }()
+
+    private lazy var mouthHeight: Float = {
+        let (min, max) = mouthNode.boundingBox
+        return max.y - min.y
+    }()
+
+    private lazy var pupilWidth: Float = {
+        let (min, max) = pupilRightNode.boundingBox
+        return max.x - min.x
+    }()
+
+    private lazy var pupilHeight: Float = {
+        let (min, max) = pupilRightNode.boundingBox
+        return max.y - min.y
+    }()
+
+    private lazy var maxPupilTravel: Float = { (pupilWidth + pupilHeight) * 0.5 }()
 
     // MARK: - Initialization
 
@@ -38,6 +79,11 @@ class Pig: SCNNode {
         addChildNode(node)
 
         neutralBrowY = browNode.position.y
+        neutralMouthY = mouthNode.position.y
+        neutralLeftPupilX = pupilLeftNode.position.x
+        neutralLeftPupilY = pupilLeftNode.position.y
+        neutralRightPupilX = pupilRightNode.position.x
+        neutralRightPupilY = pupilRightNode.position.y
     }
 
     required init?(coder: NSCoder) {
@@ -54,10 +100,37 @@ class Pig: SCNNode {
 
     private var blendShapes: [ARFaceAnchor.BlendShapeLocation: Any] = [:] {
         didSet {
-            guard let browUp = blendShapes[.browInnerUp] as? Float else { return }
+            guard let browUp = blendShapes[.browInnerUp] as? Float,
+                let mouthOpen = blendShapes[.jawOpen] as? Float,
 
-            let browHeight = browNode.boundingBox.max.y - browNode.boundingBox.min.y
+                let eyeLookInLeft = blendShapes[.eyeLookInLeft] as? Float,
+                let eyeLookOutLeft = blendShapes[.eyeLookOutLeft] as? Float,
+                let eyeLookUpLeft = blendShapes[.eyeLookUpLeft] as? Float,
+                let eyeLookDownLeft = blendShapes[.eyeLookDownLeft] as? Float,
+
+                let eyeBlinkLeft = blendShapes[.eyeBlinkLeft] as? Float,
+                let eyeLookInRight = blendShapes[.eyeLookInRight] as? Float,
+                let eyeLookOutRight = blendShapes[.eyeLookOutRight] as? Float,
+                let eyeLookUpRight = blendShapes[.eyeLookUpRight] as? Float,
+                let eyeLookDownRight = blendShapes[.eyeLookDownRight] as? Float,
+                let eyeBlinkRight = blendShapes[.eyeBlinkRight] as? Float else { return }
+
             browNode.position.y = neutralBrowY + browUp * browHeight
+
+            mouthNode.position.y = neutralMouthY - mouthOpen * mouthHeight
+
+            pupilLeftNode.position = SCNVector3(
+                neutralLeftPupilX - maxPupilTravel * (eyeLookInLeft - eyeLookOutLeft),
+                neutralLeftPupilY - maxPupilTravel * (eyeLookDownLeft - eyeLookUpLeft),
+                pupilLeftNode.position.z)
+
+            pupilRightNode.position = SCNVector3(
+                neutralRightPupilX + maxPupilTravel * (eyeLookInRight - eyeLookOutRight),
+                neutralRightPupilY - maxPupilTravel * (eyeLookDownRight - eyeLookUpRight),
+                pupilLeftNode.position.z)
+
+            eyeLeftNode.scale.y = 1.0 - eyeBlinkLeft
+            eyeRightNode.scale.y = 1.0 - eyeBlinkRight
         }
     }
 
