79005161b387a51d550965ce5385cdbc688c01c9
Connor Power <connor@connorpower.com>
2020-01-18 21:38:29 +0000

    Tidy default project code

--- ARPigFlip/ARPigFlip/ViewController.swift
+++ ARPigFlip/ARPigFlip/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 import RealityKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var arView: ARView!
-    
+
+    // MARK: - IBOutlets
+
+    @IBOutlet private var arView: ARView!
+
+    // MARK: - View Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
+
         let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
     }
+
 }
