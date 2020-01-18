5d78bdf0c6cefdefaa33b7b38989376ca6ebbc23
Connor Power <connor@connorpower.com>
2020-01-18 21:19:11 +0000

    Initial commit

---
+++ ARPigFlip/ARPigFlip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARPigFlip
+//
+//  Created by Connor Power on 18/01/2020.
+//  Copyright © 2020 Connor Power. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
